fcdc324a70985e1b6019744b0205b9e0103693e1
MoriAoto <k024g0051@g.neec.ac.jp>
2025-04-18 17:02:44 +0900

    途中

---
+++ Matrix3d.cpp
@@ -0,0 +1,164 @@
+﻿#include "Matrix3d.h"
+#include <cmath>
+#include <Novice.h>
+
+// 1.X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result;
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+// 2.Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result;
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = -std::sin(radian);
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = std::sin(radian);
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+// 3.Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result;
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result.m[j][i] = 0;
+			for (int k = 0; k < 4; ++k) {
+				result.m[j][i] += m1.m[j][k] * m2.m[k][i];
+			}
+		}
+	}
+	return result;
+}
+
+// 1. 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result.m[j][i] = 0.0f;
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		result.m[i][i] = 1.0f;
+	}
+
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+
+	return result;
+}
+
+// 2. 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
+	Matrix4x4 result;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			result.m[j][i] = 0.0f;
+		}
+	}
+
+	result.m[0][0] = scale.x;
+	result.m[1][1] = scale.y;
+	result.m[2][2] = scale.z;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+	Matrix4x4 scaleRot = Multiply(rotateXYZMatrix, scaleMatrix);
+	Matrix4x4 resultMatrix = Multiply(scaleRot, translateMatrix);
+
+	return resultMatrix;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWide = 60;
+// 4x4行列の数値表示関数
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name) {
+	Novice::ScreenPrintf(x, y, "%s", name);
+
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWide,
+				y + 20 + row * kRowHeight,
+				"%6.02f",
+				matrix.m[row][column]);
+		}
+	}
+}
---
+++ Matrix3d.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+struct Vector3 {
+	float x, y, z;
+};
+
+// <summary>
+/// 1.X軸回転行列関数
+/// </summary>
+/// <param name="radian">ラジアン</param>
+/// <returns>X軸回転行列</returns>
+Matrix4x4 MakeRotateXMatrix(float radian);
+
+/// <summary>
+/// 2.Y軸回転行列関数
+/// </summary>
+/// <param name="radian">ラジアン</param>
+/// <returns>Y軸回転行列</returns>
+Matrix4x4 MakeRotateYMatrix(float radian);
+
+/// <summary>
+/// 3.Z軸回転行列関数
+/// </summary>
+/// <param name="radian">ラジアン</param>
+/// <returns>Z軸回転行列</returns>
+Matrix4x4 MakeRotateZMatrix(float radian);
+
+/// <summary>
+/// 行列の積を求める関数
+/// </summary>
+/// <param name="m1">行列1</param>
+/// <param name="m2">行列2</param>
+/// <returns>行列の積</returns>
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+/// <summary>
+///  1. 平行移動行列関数
+/// </summary>
+/// <param name="translate">方向</param>
+/// <returns>平行移動行列</returns>
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+/// <summary>
+/// 2. 拡大縮小行列関数
+/// </summary>
+/// <param name="scale">スケール</param>
+/// <returns>拡大縮小行列</returns>
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+/// <summary>
+/// 行列の数値を表示する関数
+/// </summary>
+/// <param name="x">X座標</param>
+/// <param name="y">Y座標</param>
+/// <param name="matrix">行列</param>
+/// <param name="name">行列名</param>
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name);
