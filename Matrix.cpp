0191e027bfb87bfbb500019872eb1cd55eb1a7f3
MoriAoto <k024g0051@g.neec.ac.jp>
2025-04-25 16:11:19 +0900

    課題完了

---
+++ Matrix.cpp
@@ -0,0 +1,104 @@
+﻿#include "Matrix.h"
+#include <Novice.h>
+#include <cmath>
+
+
+// 正射影行列の作成関数
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 orthoMatrix;
+
+	orthoMatrix.m[0][0] = 2.0f / (right - left);
+	orthoMatrix.m[0][1] = 0.0f;
+	orthoMatrix.m[0][2] = 0.0f;
+	orthoMatrix.m[0][3] = 0.0f;
+
+	orthoMatrix.m[1][0] = 0.0f;
+	orthoMatrix.m[1][1] = 2.0f / (top - bottom);
+	orthoMatrix.m[1][2] = 0.0f;
+	orthoMatrix.m[1][3] = 0.0f;
+
+	orthoMatrix.m[2][0] = 0.0f;
+	orthoMatrix.m[2][1] = 0.0f;
+	orthoMatrix.m[2][2] = 1.0f / (farClip - nearClip);
+	orthoMatrix.m[2][3] = 0.0f;
+
+	orthoMatrix.m[3][0] = (left + right) / (left - right);
+	orthoMatrix.m[3][1] = (top + bottom) / (bottom - top);
+	orthoMatrix.m[3][2] = nearClip / (nearClip - farClip);
+	orthoMatrix.m[3][3] = 1.0f;
+
+	return orthoMatrix;
+}
+
+
+// 透視投影行列の作成関数
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 perspectiveFovMatrix;
+
+	perspectiveFovMatrix.m[0][0] = (1.0f / aspectRatio) * 1.0f / tan(fovY / 2.0f);
+	perspectiveFovMatrix.m[0][1] = 0.0f;
+	perspectiveFovMatrix.m[0][2] = 0.0f;
+	perspectiveFovMatrix.m[0][3] = 0.0f;
+
+	perspectiveFovMatrix.m[1][0] = 0.0f;
+	perspectiveFovMatrix.m[1][1] = 1.0f / tan(fovY / 2.0f);
+	perspectiveFovMatrix.m[1][2] = 0.0f;
+	perspectiveFovMatrix.m[1][3] = 0.0f;
+
+	perspectiveFovMatrix.m[2][0] = 0.0f;
+	perspectiveFovMatrix.m[2][1] = 0.0f;
+	perspectiveFovMatrix.m[2][2] = farClip / (farClip - nearClip);
+	perspectiveFovMatrix.m[2][3] = 1.0f;
+
+	perspectiveFovMatrix.m[3][0] = 0.0f;
+	perspectiveFovMatrix.m[3][1] = 0.0f;
+	perspectiveFovMatrix.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+	perspectiveFovMatrix.m[3][3] = 1.0f;
+
+	return perspectiveFovMatrix;
+}
+
+
+// ビューポートに変換する関数
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 viewportMatrix;
+
+	viewportMatrix.m[0][0] = width / 2.0f;
+	viewportMatrix.m[0][1] = 0.0f;
+	viewportMatrix.m[0][2] = 0.0f;
+	viewportMatrix.m[0][3] = 0.0f;
+
+	viewportMatrix.m[1][0] = 0.0f;
+	viewportMatrix.m[1][1] = -(height / 2.0f);
+	viewportMatrix.m[1][2] = 0.0f;
+	viewportMatrix.m[1][3] = 0.0f;
+
+	viewportMatrix.m[2][0] = 0.0f;
+	viewportMatrix.m[2][1] = 0.0f;
+	viewportMatrix.m[2][2] = maxDepth - minDepth;
+	viewportMatrix.m[2][3] = 0.0f;
+
+	viewportMatrix.m[3][0] = left + (width / 2.0f);
+	viewportMatrix.m[3][1] = top + (height / 2.0f);
+	viewportMatrix.m[3][2] = minDepth;
+	viewportMatrix.m[3][3] = 1.0f;
+
+	return viewportMatrix;
+}
+
+static const int kRowHeight = 20;
+static const int kColumnWide = 60;
+// 行列の数値を表示する関数
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name) {
+	Novice::ScreenPrintf(x, y, "%s", name);
+
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWide,
+				y + 20 + row * kRowHeight,
+				"%6.02f",
+				matrix.m[row][column]);
+		}
+	}
+}
---
+++ Matrix.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+struct Matrix4x4 {
+	float m[4][4];
+};
+
+struct Vector3 {
+	float x, y, z;
+};
+
+/// <summary>
+/// 正射影行列の作成関数
+/// </summary>
+/// <param name="left">左上のX座標</param>
+/// <param name="top">左上のY座標</param>
+/// <param name="right">右下のX座標</param>
+/// <param name="bottom">右下のY座標</param>
+/// <returns>正射影行列</returns>
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+/// <summary>
+/// 透視投影行列の作成関数
+/// </summary>
+/// <param name="fovY">画角Y</param>
+/// <param name="aspectRatio">アスペクト比</param>
+/// <param name="nearClip">近平面への距離</param>
+/// <param name="farClip">遠平面への距離</param>
+/// <returns>透視投影行列</returns>
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+/// <summary>
+/// ビューポートに変換する関数
+/// </summary>
+/// <param name="left">左上のX座標</param>
+/// <param name="top">左上のY座標</param>
+/// <param name="width">右下のX座標</param>
+/// <param name="height">右下のY座標</param>
+/// <param name="minDepth">最小深度値</param>
+/// <param name="maxDepth">最大深度値</param>
+/// <returns>ビューポート行列</returns>
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
+/// <summary>
+/// 行列の数値を表示する関数
+/// </summary>
+/// <param name="x">X座標</param>
+/// <param name="y">Y座標</param>
+/// <param name="matrix">行列</param>
+/// <param name="name">行列名</param>
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* name);
